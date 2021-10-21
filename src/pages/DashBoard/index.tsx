cfa6ef7011d2c43aa1ea328067bfcaed0ca97a64
Daniel Pires <dfpires@gmail.com>
2021-10-20 21:37:24 -0300

    20-10

--- src/pages/DashBoard/index.tsx
+++ src/pages/DashBoard/index.tsx
@@ -3,14 +3,38 @@ import { Form, Repos, Title } from './styles'
 
 import logo from '../../assets/logo.svg'
 
+
 export const Dashboard: React.FC = () => {
+
+    // criando uma interface - criei um tipo de dados
+    interface GitHubRepository {
+        full_name: string;
+        description: string;
+        owner: {
+            login: string;
+            avatar_url: string;
+        }
+    }
+
+    // cria variável e já define o método que altera esta variável
+    // o useState altera o valor da variável para vazio
+    const [newRepository, setNewRepository] = React.useState('')
+
+    // função chamada quando a caixa de texto for alterada
+    // event representa o elemento HTML que sofreu o evento
+    function handleInputChange(event: React.ChangeEvent<HTMLInputElement>): void {
+        // altera a variável newRepository
+        // target -> alvo
+        setNewRepository(event.target.value) // valor do elemento alvo que sofreu o evento
+    }   
+
     // utilizando o componente estilizado
     return (
         <>
             <img src={logo} alt="GitCollection"/>
             <Title> Dashboard </Title>
-            <Form>
-                <input placeholder="username/repository_name"/>
+            <Form> {/* quando a caixa de texto sofrer alteração, o método handleInputChange será executado */}
+                <input onChange={handleInputChange} placeholder="username/repository_name"/>
                 <button type="submit"> Buscar </button>
             </Form>
             <Repos>
