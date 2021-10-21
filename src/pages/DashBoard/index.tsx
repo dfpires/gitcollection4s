046e3b4223537908fda19f36a7028c3b80525669
Daniel Pires <dfpires@gmail.com>
2021-10-20 22:00:35 -0300

    08-10

--- src/pages/DashBoard/index.tsx
+++ src/pages/DashBoard/index.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { Form, Repos, Title } from './styles'
 
 import logo from '../../assets/logo.svg'
-
+import {api} from '../../services/api'
 
 export const Dashboard: React.FC = () => {
 
-    // criando uma interface - criei um tipo de dados
+    // criando uma interface - criei um tipo de dados que minimiza todos os campos trazidos do github
     interface GitHubRepository {
         full_name: string;
         description: string;
@@ -19,6 +19,8 @@ export const Dashboard: React.FC = () => {
     // cria variável e já define o método que altera esta variável
     // o useState altera o valor da variável para vazio
     const [newRepository, setNewRepository] = React.useState('')
+    // repositories é uma lista de GitHubRepositories, que inicialmente será uma lista vazia
+    const [repositories, setRepositories] = React.useState<GitHubRepository[]>([])
 
     // função chamada quando a caixa de texto for alterada
     // event representa o elemento HTML que sofreu o evento
@@ -28,12 +30,26 @@ export const Dashboard: React.FC = () => {
         setNewRepository(event.target.value) // valor do elemento alvo que sofreu o evento
     }   
 
+    async function handleAddRepository(event: React.FormEvent<HTMLFormElement>, ): Promise<void>{
+        // não recarrega a página
+        event.preventDefault();
+        // chama a api passando o nome do repositório a ser buscado
+        // retorna o resultado
+        const response = await api.get<GitHubRepository>(`repos/${newRepository}`)
+        const repository = response.data // dados da resposta
+        // adicionar à lista de repositórios o novo repositório que veio
+        // spread ... recupera todos os repositórios da lista e adiciona o novo
+        setRepositories([...repositories, repository])
+        // limpa o repositório atual - para limpar caixa de texto
+        setNewRepository('')
+    }
     // utilizando o componente estilizado
     return (
         <>
             <img src={logo} alt="GitCollection"/>
             <Title> Dashboard </Title>
-            <Form> {/* quando a caixa de texto sofrer alteração, o método handleInputChange será executado */}
+            {/* quando usuário clicar no botão, a função handleAddRepository será chamada */}
+            <Form onSubmit={handleAddRepository}> {/* quando a caixa de texto sofrer alteração, o método handleInputChange será executado */}
                 <input onChange={handleInputChange} placeholder="username/repository_name"/>
                 <button type="submit"> Buscar </button>
             </Form>
