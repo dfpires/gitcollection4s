51bd2201fec2ecbd890162e614cfbf88b4b67267
Daniel Pires <dfpires@gmail.com>
2021-10-27 21:51:01 -0300

    27-10

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -3,6 +3,7 @@ import { FiChevronsLeft } from 'react-icons/fi';
 import { useRouteMatch } from 'react-router'
 import { Link } from 'react-router-dom';
 import logo from '../../assets/logo.svg'
+import { Header } from './styles';
 
 // criando um tipo de dado - é o dado do parâmetro da rota
 interface RepositoryParams {
@@ -26,6 +27,29 @@ export const Repository: React.FC = (props) => {
             Voltar
           </Link>
       </Header>
+      <RepoInfo>
+        <header>
+          <img src="..." alt="..."/>
+          <div>
+            <strong> nome completo do repositório </strong>
+            <p> descrição do repositório </p>
+          </div>
+        </header>
+        <ul>
+          <li> 
+            <strong> quantas estrelas </strong>
+            <span> Stars </span>
+          </li>
+          <li> 
+            <strong> quantos forks </strong>
+            <span> Forks </span>
+          </li>
+          <li>
+            <strong> qtde de problemas </strong>
+            <span> Issues abertas </span>
+          </li>
+        </ul>
+      </RepoInfo>
     </>
   )
 }
