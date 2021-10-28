0150309a6fd00d3796d57a8d958273a98f9eb54b
Daniel Pires <dfpires@gmail.com>
2021-10-27 21:41:29 -0300

    27-10

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,12 +1,31 @@
 import React from 'react'
+import { FiChevronsLeft } from 'react-icons/fi';
+import { useRouteMatch } from 'react-router'
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.svg'
 
-interface IParametros {
-    nome: string;
-    email: string;
+// criando um tipo de dado - é o dado do parâmetro da rota
+interface RepositoryParams {
+    repository: string;
 }
 // props representa as propriedas dos componentes
-export const Repository: React.FC<IParametros> = (props) => {
+export const Repository: React.FC = (props) => {
+
+  // cria uma variável que representa o parâmetro vindo na rota
+  // que é do tipo RepositoryParams
+  const {params} = useRouteMatch<RepositoryParams>();
+
 
    //  return (<h2> Repositório  Nome: {props.nome} Email: {props.email}</h2>)
-  return (<h2 className="repo"> Repositório  </h2>)
+  return (
+    <>
+      <Header>
+          <img src={logo} alt="GitCollection"/>
+          <Link to="/">
+            <FiChevronsLeft /> {/* arte com seta para esquerda*/}  
+            Voltar
+          </Link>
+      </Header>
+    </>
+  )
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -12,7 +12,10 @@ export const Routes: React.FC = () => {
         
         <Switch>
             <Route component={Dashboard} path="/" exact/> { /* DashBoard é o componente raiz e a página padrão da aplicação */}
-            <Route component={Repository} path="/repositories"/> {/* criando a rota para os repositórios */}
+            {/* :repository é como se fosse uma variável, que vamos usar para trocar 
+            dados entre componentes 
+            colocamos o + para indicar que a barra não faz parte da rota ou da URL */}
+            <Route component={Repository} path="/repositories/:repository+"/> {/* criando a rota para os repositórios */}
         </Switch>
     )
 }
