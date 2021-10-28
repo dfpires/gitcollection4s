24747a218a89b15cb6b01795424d500916947c74
Daniel Pires <dfpires@gmail.com>
2021-10-27 21:59:28 -0300

    27-10

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -3,18 +3,32 @@ import { FiChevronsLeft } from 'react-icons/fi';
 import { useRouteMatch } from 'react-router'
 import { Link } from 'react-router-dom';
 import logo from '../../assets/logo.svg'
-import { Header } from './styles';
+import { Header, RepoInfo } from './styles';
 
 // criando um tipo de dado - é o dado do parâmetro da rota
 interface RepositoryParams {
     repository: string;
 }
+
+interface GitHubRepository {
+  full_name: string;
+  description: string;
+  forks_count: number;
+  open_issues_count: number;
+  stargazers_count: number;
+  owner: {
+      login: string;
+      avatar_url: string;
+  }
+}
+
 // props representa as propriedas dos componentes
 export const Repository: React.FC = (props) => {
 
   // cria uma variável que representa o parâmetro vindo na rota
   // que é do tipo RepositoryParams
   const {params} = useRouteMatch<RepositoryParams>();
+
 
 
    //  return (<h2> Repositório  Nome: {props.nome} Email: {props.email}</h2>)
